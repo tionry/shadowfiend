91044eeb60e08f84d2393bdb97b473a4ffd83ea9
nezharen <nezharen@163.com>
2014-10-09 12:56:25 +0800

    model runner haskell

--- models/runner.js
+++ models/runner.js
@@ -79,7 +79,7 @@ function Runner(name, type, src){
 				break;
             case 'hs':
                 that.script = [
-                    {cmd:'ghc', args:['-v0 -o', that.name + '.out', that.name]},
+                    {cmd:'ghc', args:['-v0', '-o', that.name + '.out', that.name]},
                     {cmd:A, args:['0', that.name + '.out'], start:true}
                 ];
                 break;
