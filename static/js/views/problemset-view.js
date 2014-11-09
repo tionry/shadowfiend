5fc1d2b80c9b0cde970ef892d37ae1778710fddd
zouda <zhfthss@126.com>
2014-11-09 19:04:50 +0800

    problem-set

--- static/js/views/problemset-view.js
+++ static/js/views/problemset-view.js
@@ -3,8 +3,34 @@ var app = app || {};
 (function(){
     'use strict' ;
     app.ProblemsetView = Backbone.View.extend({
-        el:"#problemset"
+        el:"#problemset",
+        events: {
+            'click #btn_newprogramconfirm':'createproblem'
+        },
+
+        createproblem: function(){
+            var name = $('#newproblem-name').val();
+            var description = $('#newproblem-description').val();
+            if (app.Lock.attach({
+                    loading: '#newproblem-buttons',
+                    error: function (data) {
+                        //app.showMessageInDialog('#changepassword', data.err, 0);
+                    },
+                    success: function () {
+                        $('#newproblem').modal('hide');
+                        //app.showMessageBox('changepassword', 'changepassworddone');
+                    },
+                })) {
+                alert('done!');
+                app.socket.emit('add-problem', {
+                    name: name,
+                    description: description,
+                });
+            }
+        }
     });
+
+
     app.init || (app.init = {});
     app.init.ProblemsetView = function () {
         if (app.views['problemset']) {
