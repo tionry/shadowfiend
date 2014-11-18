4ec059354f7bef553b1a7136d320eafc87eb42e3
zouda <zhfthss@126.com>
2014-11-18 20:52:00 +0800

    problem createtime shown

--- static/js/views/problemset-view.js
+++ static/js/views/problemset-view.js
@@ -88,7 +88,7 @@ var app = app || {};
             if (app.Lock.attach({
                     loading: '#newproblem-buttons',
                     error: function (data) {
-                       // app.showMessageBar(id, data.err, 'error');
+                       alert("add problem error!");
                     },
                     success: function () {
                         modal.modal('hide');
