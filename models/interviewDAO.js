dd6f1a4945cf818e4623e8495fbba298f41e01e9
tionry <tionry@163.com>
2014-11-26 17:00:15 +0800

    interviewDAO modify modify interviewer list or interviewee list

--- models/interviewDAO.js
+++ models/interviewDAO.js
@@ -168,7 +168,7 @@ InterviewDAO.prototype.changestatus = function(name, status){
 };
 
 InterviewDAO.prototype.updateIntervieweestatus = function(interviewname, intervieweename,status, callback) {
-    lock.acquire(name, function() {
+    lock.acquire(interviewname, function() {
         var interv = db.interview.find({name:interviewname});
         var i = 0,index = -1;
         interv.interviewee.forEach(function(iname,i){
@@ -191,21 +191,21 @@ InterviewDAO.prototype.updateIntervieweestatus = function(interviewname, intervi
 
         }, function(err, interview) {
             if (err) {
-                lock.release(name);
+                lock.release(interviewname);
                 return callback("inner error");
             }
             if (!interview) {
-                lock.release(name);
+                lock.release(interviewname);
                 return callback("interview not found");
             }
-            lock.release(name);
+            lock.release(interviewname);
             return callback(null, interview);
         });
     });
 };
 
 InterviewDAO.prototype.updateInterviewstatus = function(interviewname,status, callback) {
-    lock.acquire(name, function() {
+    lock.acquire(interviewname, function() {
         db.interview.update(
             {name: interviewname},
             {
@@ -215,14 +215,68 @@ InterviewDAO.prototype.updateInterviewstatus = function(interviewname,status, ca
 
             }, function(err, interview) {
                 if (err) {
-                    lock.release(name);
+                    lock.release(interviewname);
                     return callback("inner error");
                 }
                 if (!interview) {
-                    lock.release(name);
+                    lock.release(interviewname);
                     return callback("interview not found");
                 }
-                lock.release(name);
+                lock.release(interviewname);
+                return callback(null, interview);
+            });
+    });
+};
+
+InterviewDAO.prototype.modifyinterviewers = function(interviewname,interviewers,callback){
+    lock.acquire(interviewname, function() {
+        db.interview.update(
+            {name: interviewname},
+            {
+                $set:{
+                    interviewer:interviewers
+                }
+
+            }, function(err, interview) {
+                if (err) {
+                    lock.release(interviewname);
+                    return callback("inner error");
+                }
+                if (!interview) {
+                    lock.release(interviewname);
+                    return callback("interview not found");
+                }
+                lock.release(interviewname);
+                return callback(null, interview);
+            });
+    });
+};
+
+InterviewDAO.prototype.modifyinterviewees = function(interviewname,interviewees,callback){
+    lock.acquire(interviewname, function() {
+        var intervieweelist = [];
+        var i = 0;
+        interviewees.forEach(function(iname,i){
+            intervieweelist[i] = {name:iname,status:"waiting"};
+            i++;
+        });
+        db.interview.update(
+            {name: interviewname},
+            {
+                $set:{
+                    interviewee:intervieweelist
+                }
+
+            }, function(err, interview) {
+                if (err) {
+                    lock.release(interviewname);
+                    return callback("inner error");
+                }
+                if (!interview) {
+                    lock.release(interviewname);
+                    return callback("interview not found");
+                }
+                lock.release(interviewname);
                 return callback(null, interview);
             });
     });
