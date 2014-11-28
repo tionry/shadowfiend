5aac20281de4b55470d686b79d44d8e0a41394ca
zouda <zhfthss@126.com>
2014-11-28 23:29:14 +0800

    fix bug in adding new interview problem

--- static/js/views/interviewer-main-view.js
+++ static/js/views/interviewer-main-view.js
@@ -112,7 +112,7 @@ var app = app || {};
         },
 
         add_problem: function(){
-            var itvname = this.itv.name;
+            var itvname = $('#interviewer-item-name').text();
             var modal = Backbone.$('#set-problem');
             app.showInputModal(modal);
             var ap = modal.find('#setproblem-add'),
