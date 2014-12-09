f7ae48f17c95cb0f36ac55befc3c28cda0b02d0b
tionry <tionry@163.com>
2014-12-09 22:55:26 +0800

    de some bugs

--- models/interviewDAO.js
+++ models/interviewDAO.js
@@ -225,7 +225,7 @@ InterviewDAO.prototype.updateIntervieweestatus = function(interviewname, intervi
 
 InterviewDAO.prototype.updateProblemstatus = function(interviewname, problemname,status, callback) {
     lock.acquire(interviewname, function() {
-        db.interview.findOne({name:interviewname},{interviewee:1},function(err,interv){
+        db.interview.findOne({name:interviewname},{problemlist:1},function(err,interv){
             if(err){
                 lock.release(interviewname);
                 return callback("inner error");
