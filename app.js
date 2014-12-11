80ec204d6ccf1d376e6edd9706cf5081e28c13de
tarma <tarma_fu@hotmail.com>
2014-12-11 22:13:51 +0800

    Delete callback

--- app.js
+++ app.js
@@ -1286,7 +1286,7 @@ io.sockets.on('connection', function(socket){
 	});
 
 	socket.on('get-status-interviewees', function(data) {
-		if (!check(data, 'interviewName', 'status', 'callback')) {
+		if (!check(data, 'interviewName', 'status')) {
 			return;
 		}
 		if (!socket.session) {
