88e94af599e8a2ae891223448296c6db5509ed81
zouda <zhfthss@126.com>
2014-12-12 23:52:39 +0800

    empty list fixed

--- static/js/views/interviewer-main-view.js
+++ static/js/views/interviewer-main-view.js
@@ -127,7 +127,7 @@ var app = app || {};
         },
 
         resetProblem : function(){
-            this.options.pushedProblem(this.renewProblem);
+            this.options.pushedProblem.each(this.renewProblem);
         },
 
         renew_running_interview: function(){
