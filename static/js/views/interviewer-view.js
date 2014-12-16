7990dc8bfbd68386da73aca04a7596ce71a66207
tarma <tarma_fu@hotmail.com>
2014-12-16 21:23:27 +0800

    Try to broadcast

--- static/js/views/interviewer-view.js
+++ static/js/views/interviewer-view.js
@@ -27,9 +27,6 @@ var app = app || {};
                 });
                 $('#interviewers-control-table').append(model.view.render().el);
             }
-            if (model.v) {
-                model.v.renewView();
-            }
             return this;
         },
 
