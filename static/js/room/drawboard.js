d90447221ed7a347f01728d4d35f3034a5718ff2
zouda <zhfthss@126.com>
2014-12-18 21:37:38 +0800

    add drawboard

--- static/js/room/drawboard.js
+++ static/js/room/drawboard.js
@@ -3,9 +3,16 @@ var app = app || {};
 /* 房间绘图板控制器 */
 app.Room && _.extend(app.Room.prototype, {
 
+    initBoard: function(){
+        var path = this.docModel.attributes.path;
+    },
+
     onSavingDraw: function(data){
         var path = this.docModel.attributes.path;
-
+        app.socket.emit('save-image', {
+            fileName: path,
+            image: data,
+        });
     },
 
     afterRevision: function(data){
