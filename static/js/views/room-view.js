ed4060d3bf122f49f6b980223cf337593c28ca13
zouda <zhfthss@126.com>
2014-12-18 22:53:38 +0800

    add drawboard

--- static/js/views/room-view.js
+++ static/js/views/room-view.js
@@ -474,7 +474,7 @@ var app = app || {};
 
         renewDraw: function(data){
             var canvas = $('.drawing-board-canvas')[0];
-            canvas.drawImage(data, 0, 0);
+            canvas.getContext("2d").drawImage(data, 0, 0);
         },
 
         drawboard: function(){
