056074d4839d69fc67bdba402a679dab532ba991
zouda <zhfthss@126.com>
2014-12-20 13:13:52 +0800

    bind line comment with line handle

--- static/js/room/comment.js
+++ static/js/room/comment.js
@@ -21,12 +21,12 @@ app.Room && _.extend(app.Room.prototype, {
     //刷新批注
     reloadComment: function(){
         var view = app.room.view;
-        view.clearAllLineWidget();
-        view.inpopover = false;
-        for (var i = 0; i < view.editor.lineCount(); i++){
-            var msg = view.setLineWidget();
-            view.editor.addLineWidget(i, msg[0], {coverGutter: false, noHScroll: true});
-        }
+        //view.clearAllLineWidget();
+        //view.inpopover = false;
+        //for (var i = 0; i < view.editor.lineCount(); i++){
+        //    var msg = view.setLineWidget();
+        //    view.editor.addLineWidget(i, msg[0], {coverGutter: false, noHScroll: true});
+        //}
     },
 
     //增加批注
