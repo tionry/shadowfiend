662d570b6fabf9ad53824a43a2ac5844876c8293
zouda <zhfthss@126.com>
2014-12-22 23:46:11 +0800

    clear interviewlist

--- static/js/room/info.js
+++ static/js/room/info.js
@@ -5,14 +5,14 @@ app.Room && _.extend(app.Room.prototype, {
 
     //停止某道题目作答
     stopOneProblem: function(){
-        if (window.location.hash == '#edit'){
+        if (window.location.hash == '#edit/'){
             var modal = $('#stopprobleminfo');
             app.showInputModal(modal);
         }
     },
 
     startOneProblem: function(){
-        if (window.location.hash == '#edit'){
+        if (window.location.hash == '#edit/'){
             var modal = $('#stopprobleminfo');
             modal.modal('hide');
         }
