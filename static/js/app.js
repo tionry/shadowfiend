8b5da89199435c06c6ed716cd72d38cffdb63a74
zouda <zhfthss@126.com>
2014-12-23 11:22:42 +0800

    界面自适应

--- static/js/app.js
+++ static/js/app.js
@@ -105,15 +105,15 @@ app.resize = function() {
 	$("#popush-info").css("margin-top", bigoneHeight + 20);
 	var topHeight = document.getElementById("nav-head").clientHeight;
 	var topWidth = document.getElementById("nav-head").clientWidth;
-	//if (topWidth >= 770){
-	//	$('.navbar-right').show();
-	//	$('.navbar-left').show();
-	//}
-	//else
-	//{
-	//	$('.navbar-right').hide();
-	//	$('.navbar-left').hide();
-	//}
+	if (topWidth >= 770){
+		$('.navbar-right').show();
+		$('.navbar-left').show();
+	}
+	else
+	{
+		$('.navbar-right').hide();
+		$('.navbar-left').hide();
+	}
 	$('#filecontrol').css("margin-top", topHeight - 5);
 	$('#editor').css("margin-top", topHeight + 5);
 	$('#problemset-control').css("margin-top", topHeight + 5);
