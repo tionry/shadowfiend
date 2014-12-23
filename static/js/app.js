953cb9e8fbc85d6d3318cc62705916fcb182cc27
zouda <zhfthss@126.com>
2014-12-24 01:13:40 +0800

    change view anim

--- static/js/app.js
+++ static/js/app.js
@@ -152,7 +152,7 @@ $(document).ready(function() {
 	});
 
 	$('#anim-btn').on('click', function(){
-		$(this).animate({left:'-200px', opacity: '0.6'}, 'slow', function(){
+		$(this).animate({left:'-400px', opacity: '0.6'}, 'slow', function(){
 			$('#anim').off('click');
 			$('#anim-login').fadeIn('fast');
 		});
